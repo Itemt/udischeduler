ba3f231c141b1bdc44a0cad6b2fe961ac02dfd5f
Christian <crisrague1@gmail.com>
2025-02-25 20:00:07 -0500

    chore: init hono project for backend

---
+++ apps/client/rsbuild.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
+import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack'
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  source: {
+    entry: { index: './src/main.tsx' },
+  },
+  html: {
+    template: './index.html',
+  },
+  tools: {
+    rspack: {
+      plugins: [
+        TanStackRouterRspack({
+          target: 'react',
+          autoCodeSplitting: true,
+        }),
+      ],
+    },
+  },
+})
